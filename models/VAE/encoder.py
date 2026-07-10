d71e55e085f61da2cba57af5e807c963c9f095f2
nitikagupta1403 <37801064+nitikagupta1403@users.noreply.github.com>
2026-07-10 17:59:24 +0530

    Update encoder.py

--- models/VAE/encoder.py
+++ models/VAE/encoder.py
@@ -1,1 +1,13 @@
+"""
+Encoder
 
+Why does this file exist?
+
+The encoder transforms an input image into a compact feature representation
+and predicts the parameters (μ and logσ²) of a latent Gaussian distribution.
+
+It does not predict the latent vector directly.
+
+Instead, it predicts a probability distribution from which the latent vector
+will later be sampled using the reparameterization trick.
+"""
