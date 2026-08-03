0dcd67e7c7f1c047dd67a5b7b180b822f0ff34fc
nitikagupta1403 <37801064+nitikagupta1403@users.noreply.github.com>
2026-08-03 18:26:28 +0530

    Update geometry.py

--- weave/geometry.py
+++ weave/geometry.py
@@ -1,4 +1,11 @@
+import numpy as np
+
+
 class Geometry:
+
+    """
+    Stores global geometric properties of a garment.
+    """
 
     def __init__(self):
 
@@ -11,10 +18,34 @@ class Geometry:
         self.width = None
         self.height = None
 
-        self.aspect_ratio = None
-
         self.area = None
+
+        self.aspect_ratio = None
 
         self.centroid = None
 
         self.edge_pixels = None
+
+    def compute(self, binary):
+
+        ys, xs = np.where(binary > 0)
+
+        self.xmin = int(xs.min())
+        self.xmax = int(xs.max())
+
+        self.ymin = int(ys.min())
+        self.ymax = int(ys.max())
+
+        self.width = self.xmax - self.xmin
+        self.height = self.ymax - self.ymin
+
+        self.area = self.width * self.height
+
+        self.aspect_ratio = self.width / self.height
+
+        self.centroid = (
+            float(xs.mean()),
+            float(ys.mean())
+        )
+
+        self.edge_pixels = len(xs)
