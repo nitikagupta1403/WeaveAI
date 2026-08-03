942fa722b4078d740a6bb340548b5a66c32a4fec
nitikagupta1403 <37801064+nitikagupta1403@users.noreply.github.com>
2026-08-03 18:24:30 +0530

    Create geometry.py

---
+++ weave/geometry.py
@@ -0,0 +1,20 @@
+class Geometry:
+
+    def __init__(self):
+
+        self.xmin = None
+        self.xmax = None
+
+        self.ymin = None
+        self.ymax = None
+
+        self.width = None
+        self.height = None
+
+        self.aspect_ratio = None
+
+        self.area = None
+
+        self.centroid = None
+
+        self.edge_pixels = None
