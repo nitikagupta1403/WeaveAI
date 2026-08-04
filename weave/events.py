8ceb45035c14827c4f2d7e1b81ad8d3f65d2942a
nitikagupta1403 <37801064+nitikagupta1403@users.noreply.github.com>
2026-08-04 22:27:37 +0530

    Update events.py

--- weave/events.py
+++ weave/events.py
@@ -2,13 +2,23 @@ import numpy as np
 
 
 class EventDetector:
+    """
+    Detect primitive geometric events
+    from a 1D geometric signal.
+    """
 
     def __init__(self, signal):
 
         self.signal = np.asarray(signal)
 
+        self.gradient = np.gradient(self.signal)
+
+        self.curvature = np.gradient(self.gradient)
+
         self.events = []
 
     def detect(self):
+
+        self.events = []
 
         return self.events
