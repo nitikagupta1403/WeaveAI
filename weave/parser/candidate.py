43fc5934b19d0abf9ffed36ccfeec439fac0696d
nitikagupta1403 <37801064+nitikagupta1403@users.noreply.github.com>
2026-08-05 13:16:02 +0530

    Create candidate.py

---
+++ weave/parser/candidate.py
@@ -0,0 +1,21 @@
+class CandidateDetector:
+
+    def __init__(self, signal):
+
+        ...
+
+    def detect(self):
+
+        boundaries = self.find_boundaries()
+
+        ...
+
+        return candidates
+
+    def find_boundaries(self):
+
+        ...
+
+    def build_candidate(...):
+
+        ...
